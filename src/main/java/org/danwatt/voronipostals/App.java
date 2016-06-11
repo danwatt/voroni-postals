078f51e5e19b79ad0d25b9565e59e9b7821ec76c
Dan Watt <danwatt@gmail.com>
2016-06-11 00:04:40 -0500

    Heroku

--- src/main/java/org/danwatt/voronipostals/App.java
+++ src/main/java/org/danwatt/voronipostals/App.java
@@ -1,11 +1,13 @@
 package org.danwatt.voronipostals;
 
 
+import org.rapidoid.setup.On;
 import org.rapidoid.web.Rapidoid;
 
 public class App {
     public static void main(String[] args) {
         PostalSource.getInstance();
+        On.port(Integer.parseInt(args[0]));
         Rapidoid.run(args);
     }
 }
